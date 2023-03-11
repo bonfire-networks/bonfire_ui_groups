8654fbf3b899af626437be04b5606c2930f724fc
Mayel de Borniol <git@mayel.space>
2023-03-11 17:25:59 +1300

    https://github.com/bonfire-networks/bonfire-app/issues/488

--- lib/components/settings/general/general_live.ex
+++ lib/components/settings/general/general_live.ex
@@ -2,5 +2,5 @@ defmodule Bonfire.UI.Groups.Settings.GeneralLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop selected_tab, :any, default: nil
-  prop group, :any, required: true
+  prop category, :any, required: true
 end
--- lib/components/settings/settings_live.ex
+++ lib/components/settings/settings_live.ex
@@ -4,7 +4,7 @@ defmodule Bonfire.UI.Groups.SettingsLive do
 
   prop selected_tab, :any, default: nil
   prop tab_id, :any, default: nil
-  prop group, :any, required: true
+  prop category, :any, required: true
   prop permalink, :any, required: true
   prop object_boundary, :any, default: nil
 
