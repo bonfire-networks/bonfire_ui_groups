beaff5e5b3bbbe98870f21a55e550b482b6c81c6
Mayel de Borniol <git@mayel.space>
2023-05-26 14:08:06 +0100

    f

--- test/views/groups_lifecycle_test.exs
+++ test/views/groups_lifecycle_test.exs
@@ -21,14 +21,19 @@ defmodule Bonfire.UI.Groups.LiveHandlerTest do
       |> render_click()
 
       group_name = "Friends"
+
       {:ok, group_view, html} =
         view
-        |> form("#new_group_form", name: group_name, summary: "test description", to_boundaries: ["visible"])
+        |> form("#new_group_form",
+          name: group_name,
+          summary: "test description",
+          to_boundaries: ["visible"]
+        )
         |> render_submit()
         |> follow_redirect(conn)
 
-        assert html =~ "Created!"
-        assert html =~ group_name
+      assert html =~ "Created!"
+      assert html =~ group_name
     end
 
     test "If I create an open group, anyone can join" do
@@ -38,16 +43,21 @@ defmodule Bonfire.UI.Groups.LiveHandlerTest do
       name = "Friends"
       description = "test description"
       to_boundaries = ["open"]
-      {:ok, group} = Bonfire.Classify.Categories.create(
-        me,
-        %{category: %{
-          name: name,
-          description: description,
-          to_boundaries: to_boundaries,
-          type: :group
-        }},
-        true
-      )
+
+      {:ok, group} =
+        Bonfire.Classify.Categories.create(
+          me,
+          %{
+            category: %{
+              name: name,
+              description: description,
+              to_boundaries: to_boundaries,
+              type: :group
+            }
+          },
+          true
+        )
+
       conn = conn(user: alice, account: account)
       next = "/&#{group.character.username}"
       {:ok, view, _html} = live(conn, next)
@@ -57,7 +67,6 @@ defmodule Bonfire.UI.Groups.LiveHandlerTest do
       assert view |> element("[data-id=follow]") |> render_click()
       # assert render(view) =~ "joined"
       # assert view |> element("[data-id=follow]") |> render_click() |> has_element?("[data-id=unfollow]")
-
     end
 
     test "If I create a visible group, anyone can request to join" do
@@ -67,16 +76,21 @@ defmodule Bonfire.UI.Groups.LiveHandlerTest do
       name = "Friends"
       description = "test description"
       to_boundaries = ["visible"]
-      {:ok, group} = Bonfire.Classify.Categories.create(
-        me,
-        %{category: %{
-          name: name,
-          description: description,
-          to_boundaries: to_boundaries,
-          type: :group
-        }},
-        true
-      )
+
+      {:ok, group} =
+        Bonfire.Classify.Categories.create(
+          me,
+          %{
+            category: %{
+              name: name,
+              description: description,
+              to_boundaries: to_boundaries,
+              type: :group
+            }
+          },
+          true
+        )
+
       conn = conn(user: alice, account: account)
       next = "/&#{group.character.username}"
       {:ok, view, _html} = live(conn, next)
@@ -85,13 +99,10 @@ defmodule Bonfire.UI.Groups.LiveHandlerTest do
       assert view |> has_element?("[data-id=follow]")
     end
 
-
     test "If I create a private group, a user with the invite can join" do
-
     end
 
     test "A private group is not visible in the local feed, even if I am a follower of the group creator" do
-
     end
 
     test "I cannot see a private group without an invite" do
@@ -101,16 +112,21 @@ defmodule Bonfire.UI.Groups.LiveHandlerTest do
       name = "Friends"
       description = "test description"
       to_boundaries = ["private"]
-      {:ok, group} = Bonfire.Classify.Categories.create(
-        me,
-        %{category: %{
-          name: name,
-          description: description,
-          to_boundaries: to_boundaries,
-          type: :group
-        }},
-        true
-      )
+
+      {:ok, group} =
+        Bonfire.Classify.Categories.create(
+          me,
+          %{
+            category: %{
+              name: name,
+              description: description,
+              to_boundaries: to_boundaries,
+              type: :group
+            }
+          },
+          true
+        )
+
       conn = conn(user: alice, account: account)
       next = "/&#{group.character.username}"
       {:ok, view, _html} = live(conn, next)
@@ -118,122 +134,86 @@ defmodule Bonfire.UI.Groups.LiveHandlerTest do
     end
 
     test "If I joined a group, I can leave it" do
-
     end
 
     test "If I joined a group, I can see the group activities" do
-
     end
 
     test "If I joined a group, I can see the group members" do
-
     end
 
     test "If I joined a group, I can see the group admins" do
-
     end
 
     test "If I joined a group, I can see the group info (description, creation date, boundary)" do
-
     end
-
   end
-
 
   describe "Publish in group" do
     test "If I joined a group, I can publish a post into it" do
-
     end
 
     test "When I publish into a group, the activity is not visible outside the group" do
-
     end
 
     test "When I publish into a group, the activity is visible in the group" do
-
     end
   end
-
 
   describe "General settings" do
     test "Group admin can create group invite links" do
-
     end
 
     test "Group admin can see group invite links" do
-
     end
 
     test "Group admin can remove group invite links" do
-
     end
 
-    test "Group admin can see requests to join the group"  do
-
+    test "Group admin can see requests to join the group" do
     end
 
     test "Group admin can accept requests to join the group" do
-
     end
 
     test "Group admin can reject requests to join the group" do
-
     end
 
-
     test "Group admin can see group settings" do
-
     end
 
     test "Group admin can edit the group general information" do
-
     end
   end
-
 
   describe "Moderate group" do
     test "Group Admin can remove a post from the group" do
-
     end
 
     test "Group Admin can remove a member from the group" do
-
     end
 
     test "Group admin can see flagged posts" do
-
     end
 
     test "Group admin can see flagged members" do
-
     end
 
     test "Group Admin can add other admins" do
-
     end
-
 
     test "Group admin can remove admins" do
-
     end
-
 
     test "Group admin can add more roles and add members to them" do
-
     end
-
   end
 
-
-
   describe "Delete group" do
-
     test "Group admin can archive group" do
-
     end
 
     test "Group admin can delete group" do
-
     end
   end
 end
