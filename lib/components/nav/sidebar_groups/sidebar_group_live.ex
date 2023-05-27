318d263b67c680a6b061f3c568151baeb40262ad
Mayel de Borniol <git@mayel.space>
2023-05-27 20:39:21 +0100

    fix

--- lib/components/nav/sidebar_groups/sidebar_group_live.ex
+++ lib/components/nav/sidebar_groups/sidebar_group_live.ex
@@ -1,6 +1,6 @@
 defmodule Bonfire.UI.Groups.SidebarGroupLive do
   use Bonfire.UI.Common.Web, :stateless_component
-  import Bonfire.UI.Me.Integration
+
   prop category, :any, required: true
   prop children, :list, default: []
 end
