d271e05a948a536388e28b9aeb287b33c3b97bc1
Mayel de Borniol <git@mayel.space>
2023-06-04 22:30:33 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/570

--- lib/components/widgets/members/widget_members_live.ex
+++ lib/components/widgets/members/widget_members_live.ex
@@ -1,7 +1,7 @@
 defmodule Bonfire.UI.Groups.WidgetMembersLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
-  prop mods, :any, default: []
+  prop moderators, :any, default: []
   prop members, :any, default: []
   prop widget_title, :string, default: nil
 end
