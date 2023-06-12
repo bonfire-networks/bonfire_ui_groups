a9d69b88496a5a3aca8f84b644bef3af29d4d9c8
Mayel de Borniol <git@mayel.space>
2023-06-12 20:42:08 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/595

--- lib/components/members/group_members_live.ex
+++ lib/components/members/group_members_live.ex
@@ -2,7 +2,7 @@ defmodule Bonfire.UI.Groups.GroupMembersLive do
   use Bonfire.UI.Common.Web, :stateless_component
   # alias Bonfire.Boundaries.Circles
 
-  # prop scope, :atom, default: nil
+  # prop scope, :any, default: nil
   # prop myself, :map, default: nil
   # prop setting_boundaries, :boolean, default: false
 end
