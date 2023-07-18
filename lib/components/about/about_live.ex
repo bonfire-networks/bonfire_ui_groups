f66eb4082f1b162288fd20fc7471583a91e7c565
ivanminutillo <ivanminutillo@outlook.it>
2023-07-18 17:50:07 +0200

    group about page

--- lib/components/about/about_live.ex
+++ lib/components/about/about_live.ex
@@ -1,10 +1,27 @@
 defmodule Bonfire.UI.Groups.AboutLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
+  prop selected_tab, :string, default: "about"
+  prop feed_title, :string
+  prop user, :map
+  prop feed, :list
+  prop feed_filters, :any, default: []
+  prop page_info, :any
+  prop permalink, :string, default: nil
+  prop showing_within, :atom, default: nil
+  prop follows_me, :boolean, default: false
+  prop loading, :boolean, default: false
+  prop hide_tabs, :boolean, default: false
+  slot header
+  slot widget
+
   prop date, :string, default: "Today"
   prop parent, :string, default: nil
   prop parent_link, :string, default: nil
   prop boundary_preset, :any, default: nil
-  prop user, :map, default: nil
   prop member_count, :integer, default: 0
+
+  prop members, :list, default: []
+  prop moderators, :list, default: []
+
 end
