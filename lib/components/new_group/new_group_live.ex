0d93ffdb9c21cfbc76a854f4415b65d53af3a223
Mayel de Borniol <git@mayel.space>
2023-08-12 12:29:28 +0100

    css

--- lib/components/new_group/new_group_live.ex
+++ lib/components/new_group/new_group_live.ex
@@ -3,5 +3,6 @@ defmodule Bonfire.UI.Groups.NewGroupLive do
 
   prop parent, :any, default: nil
   prop parent_id, :any, default: nil
+  prop open_btn_wrapper_class, :css_class, default: "flex flex-1 w-full"
   prop open_btn_class, :css_class, default: "flex items-center gap-2 text-sm text-base-content/70"
 end
