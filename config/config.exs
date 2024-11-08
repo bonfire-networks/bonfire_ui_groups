883af827f91e7ab86c99664a66b1b186f5932d16
Mayel de Borniol <git@mayel.space>
2024-11-08 19:11:47 +0000

    config cleanup

--- config/config.exs
+++ config/config.exs
@@ -21,7 +21,7 @@ config :bonfire_ui_groups, :otp_app, :bonfire_ui_groups
 config :bonfire_common, :otp_app, :bonfire_ui_groups
 config :bonfire_ui_groups, :repo_module, Bonfire.Common.Repo
 config :bonfire_ui_groups, ecto_repos: [Bonfire.Common.Repo]
-config :bonfire_ui_groups, :localisation_path, "priv/localisation"
+config :bonfire_common, :localisation_path, "priv/localisation"
 
 config :bonfire_data_identity, Bonfire.Data.Identity.Credential, hasher_module: Argon2
 
